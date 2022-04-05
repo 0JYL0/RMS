9b0c053207bfcb925cc0ff3683d774827a6f4d88
Jayal Malusare <jayalmalusare.912.jm@gmail.com>
2022-04-05 21:47:28 +0530

    Create TNR_Home.aspx.cs

---
+++ View/TNR_Home.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace test.View
+{
+    public partial class TNR_Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["Trainer_id"] == null)
+            {
+                Response.Redirect("Login.aspx");
+            }
+        }
+    }
+}
