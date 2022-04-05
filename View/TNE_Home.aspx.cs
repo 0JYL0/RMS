621856b834cf329418e155e195f8529ade071ac3
Jayal Malusare <jayalmalusare.912.jm@gmail.com>
2022-04-05 21:47:14 +0530

    Create TNE_Home.aspx.cs

---
+++ View/TNE_Home.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.Configuration;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace test.View
+{
+    public partial class TNE_Home : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(WebConfigurationManager.ConnectionStrings["rms_conn"].ConnectionString);
+        SqlCommand cmd = new SqlCommand();
+        string str;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(Session["Trainee_id"] == null)
+            {
+                Response.Redirect("Login.aspx");
+            }
+            else
+            {
+                if (!IsPostBack)
+                {
+                    load();
+                }
+            }
+        }
+
+        protected void load()
+        {
+            con.Open();
+            str = "select c.c_id, c.c_name, c.descr from member m, course c where c.c_id =m.c_id and m.TRN_Id = '"+Session["Trainee_id"]+"'";
+            cmd = new SqlCommand(str, con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+
+            da.Fill(dt);
+
+            Repeater1.DataSource = dt;
+            Repeater1.DataBind();
+            con.Close();
+        }
+    }
+}
