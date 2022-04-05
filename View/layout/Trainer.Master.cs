e418c89834c9960e75f4c303c953aadf40e19c85
Jayal Malusare <jayalmalusare.912.jm@gmail.com>
2022-04-05 21:45:58 +0530

    Create Trainer.Master.cs

---
+++ View/layout/Trainer.Master.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace test.View.layout
+{
+    public partial class Trainer : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void logout0(object sender, EventArgs e)
+        {
+            Session["Tid"] = null;
+            Response.Redirect("Login.aspx");
+        }
+    }
+}
